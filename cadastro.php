f5a78eaeae2c958c781e2d4254cad1f78fa3eeff
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-09-16 14:39:22 -0300

    login and register working in front

--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,30 @@
+<?php
+session_start();
+include "features/users.php";
+
+$error_message = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name']) && isset($_POST['user']) && isset($_POST['email']) && isset($_POST['password'])) {
+        $result = Session::register(
+            $_POST["name"],
+            $_POST["user"],
+            $_POST["email"],
+            $_POST["password"],
+        );
+
+        if ($result instanceof LoginSuccess) {
+            $_SESSION["session"] = $result->session;
+            header("Location: feed.php");
+            exit;
+        } else if ($result instanceof LoginFailure) {
+            $error_message = $result->error;
+        }
+    } else {
+        $error_message = "Parâmetros insuficientes";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,16 +41,23 @@
     <div class="form-box"> 
         <h1>Cadastrar</h1>
         <h2>Insira suas credenciais para criar uma nova conta</h2>
-        <form action="">
-            <label for="">Foto de perfil</label>
-            <input type="file" name="profile_picture" id="file-input">
+        <form action="" method="POST">
+            <!-- <label for="">Foto de perfil</label>
+            <input type="file" name="profile_picture" id="file-input"> -->
             <label for="name">Nome</label>
             <input type="text" name="name" placeholder="Nome">
+            <label for="name">Nome de Usuário</label>
+            <input type="text" name="user" placeholder="Nome de Usuário" pattern="[a-zA-Z0-9\.\-_]+">
             <label for="email">Email</label>
             <input type="email" name="email" placeholder="E-mail">
             <label for="password">Senha</label>
             <input type="password" name="password" placeholder="Senha">
             <button type="submit">Cadastrar</button>
+            <?php
+            if ($error_message) {
+                echo "<p id=\"error-message\">$error_message</p>";
+            }
+            ?>
         </form>
         <p>Já possui uma conta? <a href="index.php">Entre</a></p>
     </div>
