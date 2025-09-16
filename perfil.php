6ad009393209a5ca296bc3cc1b5d23e209854d42
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-09-16 14:09:30 -0300

    a

---
+++ perfil.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/HW-icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./styles/perfil.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>Perfil</title>
+</head>
+<body>
+
+<main class="main-content">
+    <div class="pagina-perfil">
+        <div id="fundo">
+            <img id="banner-histweb" src="img/HistWebWhite.svg" alt="">
+        </div>
+        <form class="informacoes" action="#">
+            <img id='user-perfil' src='./img/user.png' alt=''>
+            
+            <div class='info'>
+                <div class='nome-email'>
+                    <input type="hidden" name="id" value="#">
+                    <label class="nome-acima" for="upload">IMAGEM</label>
+                    <input id="upload" type="file" name="imagem" accept="image/*">
+                    <label class='nome-acima'>NOME</label>
+                    <input id='campo-nome' name="nome" type='text' value='#'>
+                    <label class='nome-acima'>EMAIL</label>
+                    <input id='campo-email' name="email" type='email' value='#'>
+                    <label class='nome-acima'>SENHA</label>
+                    <input id='campo-senha' name="senha" type='password' placeholder="Nova senha">
+                    <div id='mostrar'>
+                        <input type='checkbox' onclick='mostrarSenha()'> Mostrar senha
+                    </div>
+                    <button type="submit" id="confirma">Confirmar alterações</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</main>
+<script>
+    function mostrarSenha() {
+        var x = document.getElementById("campo-senha");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+</script>
+</body>
+</html>
