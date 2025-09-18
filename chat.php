231b404aa998bbf849428bd25597411c2ada7b52
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-09-17 23:30:28 -0300

    chat

--- chat.php
+++ chat.php
@@ -5,101 +5,80 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./img/HW-icon.png" type="image/x-icon">
     <link rel="stylesheet" href="./styles/chat.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <title>Perfil</title>
+    <title>Chat</title>
 </head>
 <body>
-
-            <div id="tudo">
-              <div class="box linha chat azul">
-                <div>
-                  <h3 class="titulo">CHAT</h3>
-                </div>
-              
-                  
-                  <div class="mensagens">
-                
-                    <div class="chat-box e">
-                      <div class="info ">
-                        <span class="nome ">liliv</span>
-                        <span class="time ">23 Jan 2:00 pm</span>
-                      </div>
-                     
-                      <img class="img" src="./img/user.png" alt="message user image">
-                   
-                      <div class="text">
-                        dghfdg hghdfg trhhdrjthg sidjkgherd jhfgisdjfkx hgi dtjkfghvnos ildtjxk hvbli ntdxkj gchn
-                      </div>
-        
-                    </div>
-
-                    <div class="chat-box right d">
-                      <div class="info ">
-                        <span class="nome ">lin</span>
-                        <span class="time ">23 Jan 2:05 pm</span>
-                      </div>
-                     
-                      <img class="img" src="./img/user.png" alt="message user image">
-                      
-                      <div class="text">
-                        sdhfkj ghdti jkfghs itdjk hgtidk jhsk
-                      </div>
-                   
-                    </div>
-                   
-
-                  
-                    <div class="chat-box e">
-                      <div class="info ">
-                        <span class="nome ">gabs</span>
-                        <span class="time ">23 Jan 5:37 pm</span>
-                      </div>
-             
-                      <img class="img" src="./img/user.png" alt="message user image">
-                     
-                      <div class="text">
-                        gdsdg jduogjd ligkj dojg skjdgkj sdgds hdfhgfx hbxgfbxg bgxbx
-                      </div>
-                    
-                    </div>
-                   
-                    <div class="chat-box right d">
-                      <div class="info ">
-                        <span class="nome ">lin</span>
-                        <span class="time ">23 Jan 6:10 pm</span>
-                      </div>
-                     
-                      <img class="img" src="./img/user.png" alt="message user image">
-          
-                      <div class="text">
-                        sdrgxfgxd sfdfa
-                      </div>
-                    
-                    </div>
+    <div class="container">
+        <div class="caixa-chat">
+            <div class="cabecalho">
+                <h3 class="titulo">CHAT</h3>
+            </div>
             
-
-                  </div>
-                 
-               
-                <div class="box-footer">
-                  <form action="#" method="post">
-                    <div class="input-group">
-                      <input type="text" name="message" placeholder="digite ..." class="form">
-                      <span class="input-group-btn">
-                            <button type="button" class="btn btn-azul">Enviar</button>
-                          </span>
+            <div class="mensagens">
+                <div class="mensagem entrada">
+                    <img class="foto" src="./img/user.png" alt="Foto do usuário">
+                    <div class="conteudo-mensagem">
+                        <div class="info">
+                            <span class="nome">liliv</span>
+                            <span class="hora">23 Jan 2:00 pm</span>
+                        </div>
+                        <div class="texto">
+                            dghfdg hghdfg trhhdrjthg sidjkgherd jhfgisdjfkx hgi dtjkfghvnos ildtjxk hvbli ntdxkj gchn
+                        </div>
                     </div>
-                  </form>
+                </div>
+
+                <div class="mensagem direita">
+                    <div class="conteudo-mensagem">
+                        <div class="info">
+                            <span class="nome">lin</span>
+                            <span class="hora">23 Jan 2:05 pm</span>
+                        </div>
+                        <div class="texto">
+                            sdhfkj ghdti jkfghs itdjk hgtidk jhsk
+                        </div>
+                    </div>
+                    <img class="foto" src="./img/user.png" alt="Foto do usuário">
+                </div>
+
+                <div class="mensagem entrada">
+                    <img class="foto" src="./img/user.png" alt="Foto do usuário">
+                    <div class="conteudo-mensagem">
+                        <div class="info">
+                            <span class="nome">gabs</span>
+                            <span class="hora">23 Jan 5:37 pm</span>
+                        </div>
+                        <div class="texto">
+                            gdsdg jduogjd ligkj dojg skjdgkj sdgds hdfhgfx hbxgfbxg bgxbx
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mensagem direita">
+                    <div class="conteudo-mensagem">
+                        <div class="info">
+                            <span class="nome">lin</span>
+                            <span class="hora">23 Jan 6:10 pm</span>
+                        </div>
+                        <div class="texto">
+                            sdrgxfgxd sfdfa
+                        </div>
+                    </div>
+                    <img class="foto" src="./img/user.png" alt="Foto do usuário">
                 </div>
             </div>
-
-           
-
-
+            
+            <div class="rodape">
+                <form action="#" method="post">
+                    <div class="grupo-input">
+                        <input type="text" name="mensagem" placeholder="digite ..." class="campo">
+                        <button type="button" class="botao botao-azul">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
