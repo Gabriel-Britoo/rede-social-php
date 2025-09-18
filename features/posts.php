d2f4ca26a7de2922fd4d2c7794b37bd70f520d21
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-09-18 09:48:12 -0300

    correction in posts

--- features/posts.php
+++ features/posts.php
@@ -2,24 +2,27 @@
 include 'php/connection.php';
 
 class Post {
-    private int $id;
-    private User $user;
-    private DateTime $created_at;
-    private string $content;
-    private bool $hasImage;
+    protected int $id;
+    protected User $user;
+    protected DateTime $created_at;
+    protected string $title;
+    protected string $content;
+    protected bool $hasImage;
 
     public function get_id(): int { return $this->id; }
     public function get_user(): User { return $this->user; }
     public function get_created_at(): DateTime { return $this->created_at; }
+    public function get_title(): string { return $this->title; }
     public function get_content(): string { return $this->content; }
     public function get_hasImage(): bool { return $this->hasImage; }
 
     public function get_image_url(): string { return "./data/post_images/{$this->id}"; }
 
-    private function __construct($id, $user, $created_at, $content, $hasImage) {
+    private function __construct($id, $user, $created_at, $title, $content, $hasImage) {
         $this->id = $id;
         $this->user = $user;
         $this->created_at = $created_at;
+        $this->title = $title;
         $this->content = $content;
         $this->hasImage = $hasImage;
     }
@@ -40,11 +43,11 @@ class Post {
      * @param int $page
      * @return Post[]
      */
-    public static function feed(Session $session, int $page): array {
+    public static function feed(Session $session, int $page = 1): array {
         global $connection;
         $userId = $session->get_id();
         $sql = "SELECT * FROM posts WHERE user_id IN (
-            SELECT user1_id WHERE user2_id = ? UNION SELECT user2_id WHERE user1_id = ? UNION SELECT ?
+            SELECT user1_id FROM user_relations WHERE user2_id = ? UNION SELECT user2_id FROM user_relations WHERE user1_id = ? UNION SELECT ?
         ) ORDER BY created_at DESC LIMIT 50 OFFSET ?";
         $stmt = $connection->prepare($sql);
         $pageOffset = $page * 50;
@@ -56,13 +59,13 @@ class Post {
         return $result->fetch_all();
     }
 
-    public static function post(Session $session, string $content, ?array $image) {
+    public static function post(Session $session, string $title, string $content, ?array $image) {
         global $connection;
-        $sql = "INSERT INTO posts (user_id, content, image) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO posts (user_id, title, content, image) VALUES (?, ?, ?, ?)";
         $stmt = $connection->prepare($sql);
         $hasImage = $image != null ? 1 : 0;
         $userId = $session->get_id();
-        $stmt->bind_param("isi", $userId, $content, $hasImage);
+        $stmt->bind_param("issi", $userId, $title, $content, $hasImage);
         $stmt->execute();
 
         if ($image) {
@@ -72,3 +75,5 @@ class Post {
         }
     }
 }
+
+Post::feed($_SESSION['session']);
