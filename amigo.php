0828ddac312eb2fdea8ebcb56bdb11a22901847c
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-09-18 11:07:55 -0300

    amigo

---
+++ amigo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Amigo</title>
+    <link rel="stylesheet" href="./styles/amigo.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="titulo">Adicionar Amigo</h1>
+        <p class="descricao">Preencha com o email para adicionar um amigo</p>
+        
+        <div class="formulario">
+
+            <div class="campo">
+                <label for="email">E-mail</label>
+                <input type="text" id="email" placeholder="Digite o e-mail do amigo">
+            </div>
+            
+            </div>
+            
+            <button class="botao-adicionar" id="botao-adicionar">Adicionar Amigo</button>
+        </div>
+    </div>
+
+</body>
+</html>
